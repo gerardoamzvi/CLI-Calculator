a81bd4aa25f385a348d2ca1800c464b49bcc1fa9
gerardoamzvi <111869677+gerardoamzvi@users.noreply.github.com>
2026-05-23 17:32:15 +0200

    add calculate function with basic operations and error handling

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+float calculate(float a, float b, char sign, int *error){
+    //*error goes to the address and reads OR writes the value
+
+    switch (sign)
+    {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/':
+            if (b == 0) {
+                printf("Error: division by zero\n");
+                *error = 1;
+                return 0;
+            }
+            return a / b;
+        default:
+            printf("Error: unknown operator\n");
+            *error = 1;
+            return 0;
+    }
+}
+
+
 int main(int argc, char *argv[])
 {   
     int number_of_word = argc - 1;
@@ -9,7 +34,6 @@ int main(int argc, char *argv[])
     I can use sizeof() to get the size of the entire array and knowing how much a single element of this array
     we can get number of element
     */
-
     printf("Number of arguments %d\n", number_of_word);
     if (number_of_word == 0)
     {
@@ -17,36 +41,19 @@ int main(int argc, char *argv[])
     }
     else
     {
-
-
+        // string argv[2] != char ;argv[2][0] (switch case works only with int and char)
+        int error = 0;
         float a = atof(argv[1]);
         float b = atof(argv[3]);
-
-        // string argv[2] != char argv[2][0] (switch case works only with int and char)
-        switch (argv[2][0])
-        {
-        case '+':
-            result = a + b;
-            break;
-        case '-':
-            result = a - b;
-            break;
-        case '*':
-            result = a * b;
-            break;
-        case '/':
-            if (b == 0) {
-                printf("Error: division by zero\n");
-            } 
-            else {
-                result = a / b;
-            }
-            break;
-        default:
-            printf("Error: unknown operator\n");
-            break;
+        char sign = argv[2][0];
+        result = calculate(a, b, sign, &error);
+        if (error){
+            printf("\n");
         }
-        printf("The result is : %f", result);
+        else{
+            printf("The result is : %f", result);
+        }
+
     }
 
     return 0;
